00ffca927bc44577706345e73c367dcdcbeae390
o0ragman0o <o0ragman0o+git@gmail.com>
2017-07-14 13:49:57 +1000

    First GH repo push

---
+++ Math.sol
@@ -0,0 +1,136 @@
+/******************************************************************************\
+
+file:   Math.sol
+ver:    0.3.0
+updated:14-July-2017
+author: Darryl Morris (o0ragman0o)
+email:  o0ragman0o AT gmail.com
+
+A simple uint overflow checking maths library.
+
+---
+pragma solidity ^0.4.11;
+
+import "Math.sol"
+
+contract K{
+    using MathU for uint;
+    uint num;
+   
+    function foo(uint x) {
+        ...
+        num = num.add(x);
+        ...
+    }
+}
+---
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See MIT Licence for further details.
+<https://opensource.org/licenses/MIT>.
+
+\******************************************************************************/
+
+pragma solidity ^0.4.11;
+
+library Math
+{
+    int8 constant LT = -1;
+    int8 constant GT = 1;
+    int8 constant EQ = 0;
+    
+    // a add to b
+    function add(uint a, uint b) internal returns (uint c) {
+        c = a + b;
+        assert(c >= a);
+    }
+    
+    // a subtract b
+    function sub(uint a, uint b) internal returns (uint c) {
+        c = a - b;
+        assert(c <= a);
+    }
+    
+    // a multiplied by b
+    function mul(uint a, uint b) internal returns (uint c) {
+        c = a * b;
+        assert(a == 0 || c / a == b);
+    }
+    
+    // a divided by b
+    function div(uint a, uint b) internal returns (uint c) {
+        c = a / b;
+        // No assert required as no overflows are posible.
+    }
+    
+    // Increment
+    function inc(uint a) internal returns (uint c) {
+        c = a + 1;
+        assert(c > a);
+    }
+    
+    // Decrement
+    function dec(uint a) internal returns (uint c) {
+        c = a - 1;
+        assert(c < a);
+    }
+    
+    // Raise a to power of b
+    function pow(uint a, uint8 b) internal returns (uint c) {
+        // still looking for a simple safe check here :(
+        c = a**b;
+    }
+
+    // Equal to
+    function eq(uint a, uint b) internal returns (bool) {
+        return a == b;
+    }
+    
+    // Less than
+    function lt(uint a, uint b) internal returns (bool) {
+        return a < b;
+    }
+    
+    // Greater than
+    function gt(uint a, uint b) internal returns (bool) {
+        return a > b;
+    }
+    
+    // Less than or equal to.
+    function lteq(uint a, uint b) internal returns (bool) {
+        return a <= b;
+    }
+    
+    // Greater than or equal to.
+    function gteq(uint a, uint b) internal returns (bool) {
+        return a >= b;
+    }
+    
+    // Zero value test
+    function isZero(uint a) internal returns (bool) {
+        return a == 0;
+    }
+    
+    // Parametric comparitor for > or <
+    // !_sym returns a < b
+    // _sym  returns a > b
+    function cmp(uint a, uint b, bool sym) internal constant returns (bool)
+    {
+        return (a!=b) && ((a < b) != sym);
+    }
+
+    // Parametric comparitor for >= or <=
+    // !_sym returns a <= b
+    // _sym  returns a >= b
+    function cmpEq(uint a, uint b, bool sym) internal constant returns (bool)
+    {
+        return (a==b) || ((a < b) != sym);
+    }
+
+    // Trinary comparitor returning int -1, 0, 1 for LT, EQ, GT
+    function cmpTri(uint a, uint b) internal returns (int8) {
+        return a < b ? LT : a > b ? GT : EQ;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# SafeMath
-A Solidity Safe Math library
+# Math
+
+A Solidity math library with overflow checking
+
+example usage:
+
+```
+pragma solidity ^0.4.11;
+
+import "Math.sol"
+
+contract K{
+    using MathU for uint;
+    uint num;
+   
+    function foo(uint x) {
+        ...
+        num = num.add(x);
+        ...
+    }
+}
+```
+
+### add
+
+a add to b
+
+`function add(uint a, uint b) internal returns (uint c)`
+
+### sub
+
+a subtract b
+
+`function sub(uint a, uint b) internal returns (uint c)`
+
+### mul
+    
+a multiplied by b
+
+`function mul(uint a, uint b) internal returns (uint c)`
+
+### div
+    
+a divided by b
+
+`function div(uint a, uint b) internal returns (uint c)`
+
+### inc
+
+Increment a
+
+`function inc(uint a) internal returns (uint c)`
+
+### dec
+
+Decrement a
+
+`function dec(uint a) internal returns (uint c)`
+
+### pow
+
+Raise a to power of b (Warning: no overflow checking)
+
+`function pow(uint a, uint8 b) internal returns (uint c)`
+
+### eq
+
+Equates a to b
+
+`function eq(uint a, uint b) internal returns (bool)`
+
+### lt
+
+a less than b
+
+`function lt(uint a, uint b) internal returns (bool)`
+
+### gt
+
+a greater than b
+
+`function gt(uint a, uint b) internal returns (bool)`
+
+### lteq
+
+a less than or equal to b
+
+`function lteq(uint a, uint b) internal returns (bool)`
+
+### gteq
+
+a greater than or equal to b
+
+`function gteq(uint a, uint b) internal returns (bool)`
+
+### isZero
+
+a is zero
+
+`function isZero(uint a) internal returns (bool)`
+
+### cmp
+
+Parametric comparitor test.
+a > b where sym == true
+a < b where sym == false
+
+`function cmp(uint a, uint b, bool sym) internal constant returns (bool)`
+
+### cmpEq
+
+Parametric comparitor test.
+a >= b where sym == true
+a <= b where sym == false
+
+`function cmpEq(uint a, uint b, bool sym) internal constant returns (bool)`
+
+### cmpTri
+
+Trinary comparitor returning int -1, 0, 1 for LT, EQ, GT
+
+`function cmpTri(uint a, uint b) internal returns (int8)`
